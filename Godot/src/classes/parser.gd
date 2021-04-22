fab122e1f5f5458c358a5c6c54606d5e0a6e4ded
Lyaaaaaaaaaaaaaaa <lyaaaaaaaaaaaaaaa@protonmail.com>
2021-04-22 15:38:03 +0200

    Created the empty scenes and files
    - main scene
    - dialogs scenes
    - interface scenes
    - parser class

---
+++ Godot/src/classes/parser.gd
@@ -0,0 +1,13 @@
+#------------------------------------------------------------------------------
+#-- Copyright (c) 2021 Lyaaaaaaaaaaaaaaa
+#--
+#-- Author : Lyaaaaaaaaaaaaaaa
+#--
+#-- Implementation Notes:
+#--  - 
+#-- Changelog:
+#--  - 22/04/2021 Lyaaaaa
+#--       - Created the empty file.
+#------------------------------------------------------------------------------
+extends Object
+class_name Parser
