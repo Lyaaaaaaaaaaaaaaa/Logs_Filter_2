76f974193fa343ea6b4d633be3348c77e0ef1781
Lyaaaaaaaaaaaaaaa <lyaaaaaaaaaaaaaaa@protonmail.com>
2021-04-27 12:28:06 +0200

    Updated file_dialog and main scenes
    - Changed some settings in file_dialog
    - Added the scenes Toolbar and TopMenu in the main scene
    - Added an empty script to main.

---
+++ Godot/src/main.gd
@@ -0,0 +1,13 @@
+#------------------------------------------------------------------------------
+#-- Copyright (c) 2021 Lyaaaaaaaaaaaaaaa
+#--
+#-- Author : Lyaaaaaaaaaaaaaaa
+#--
+#-- Implementation Notes:
+#--  -
+#-- Changelog:
+#--  - 27/04/2021 Lyaaaaa
+#--     - Created the empty file.
+#------------------------------------------------------------------------------
+extends Control
+
