f744887d514ddafcc63a669137fb12fdcd9fd376
Lyaaaaaaaaaaaaaaa <lyaaaaaaaaaaaaaaa@protonmail.com>
2021-04-30 12:09:24 +0200

    Added a checkbutton to switch case sensitivity in the tool bar
    scene:
    - Added two separators after the start button and before the
    checkbuttons.
    - Added a checkbutton to turn on/off the case sensitivity
    - Connected the signal "pressed "of the newly added checkbutton.
    
    script:
    - Added switch_case_sensitive_button_pressed signal
    - _switch_case_sensitive_button var
    - _init_variables now init _switch_case_sensitive_button
    - disable_buttons and enable_buttons now edit
    _switch_case_sensitive_button.disabled
    - Added _on_SwitchCaseSensitiveButton_pressed signal handler.

--- Godot/src/interface/tool_bar.gd
+++ Godot/src/interface/tool_bar.gd
@@ -1,40 +1,56 @@
 #------------------------------------------------------------------------------
 #-- Copyright (c) 2021 Lyaaaaaaaaaaaaaaa
 #--
-#-- Author : Lyaaaaa
+#-- Author : Lyaaaaaaaaaaaaaaa
 #--
 #-- Implementation Notes:
 #--  -
 #-- Changelog:
 #--  - 29/04/2021 Lyaaaaa
 #--    - Created the file and implemented it.
+#--
+#--  - 30/04/2021 Lyaaaaa
+#--    - Added switch_case_sensitive_button_pressed signal
+#--    - _switch_case_sensitive_button var
+#--    - _init_variables now init _switch_case_sensitive_button
+#--    - disable_buttons and enable_buttons now edit
+#--        _switch_case_sensitive_button.disabled
+#--    - Added _on_SwitchCaseSensitiveButton_pressed signal handler.
 #------------------------------------------------------------------------------
 extends Panel
 
 signal start_button_pressed
 signal switch_mode_button_pressed
+signal switch_case_sensitive_button_pressed
 
 var _start_button       : Button
 var _line_edit          : LineEdit
-var _switch_mode_button : CheckButton
+
+var _switch_mode_button           : CheckButton
+var _switch_case_sensitive_button : CheckButton
 
 func _ready() -> void:
     _init_variables()
 
+
 func disable_buttons() -> void:
-    _start_button.disabled       = true
-    _switch_mode_button.disabled = true
+    _start_button.disabled                 = true
+    _switch_mode_button.disabled           = true
+    _switch_case_sensitive_button.disabled = true
 
 
 func enable_buttons() -> void:
-    _start_button.disabled       = false
-    _switch_mode_button.disabled = false
+    _start_button.disabled                 = false
+    _switch_mode_button.disabled           = false
+    _switch_case_sensitive_button.disabled = false
 
 
 func _init_variables() -> void:
-    _start_button       = find_node("StartButton")
-    _line_edit          = find_node("LineEdit")
-    _switch_mode_button = find_node("SwitchModeButton")
+    _start_button = find_node("StartButton")
+    _line_edit    = find_node("LineEdit")
+
+    _switch_mode_button           = find_node("SwitchModeButton")
+    _switch_case_sensitive_button = find_node("SwitchCaseSensitiveButton")
 
 
 func _extract_filters() -> Array:
@@ -61,3 +77,7 @@ func _on_StartButton_pressed() -> void:
 
 func _on_SwitchModeButton_pressed() -> void:
     emit_signal("switch_mode_button_pressed")
+
+
+func _on_SwitchCaseSensitiveButton_pressed():
+    emit_signal("switch_case_sensitive_button_pressed")
