b968fec689538313a233db7f12d12e30549597cf
Lyaaaaaaaaaaaaaaa <lyaaaaaaaaaaaaaaa@protonmail.com>
2021-07-20 13:03:28 +0200

    Added a button to copy to the clipboard the displayed text.
    The button appears when text is displayed.

--- Godot/src/interface/output_display.gd
+++ Godot/src/interface/output_display.gd
@@ -8,10 +8,18 @@
 #-- Changelog:
 #--  - 29/04/2021 Lyaaaaa
 #--    - Created the file and implemented it.
+#--
+#--  - 20/07/2021 Lyaaaaa
+#--    - Added the _clipboard_button var.
+#--    - Updated append_text and clear to set visible and invisible the new
+#--        clipboard button.
+#--    - Updated _init_variables to init _clipboard_button.
+#--    - Added the _on_ClipboardButton_pressed.
 #------------------------------------------------------------------------------
 extends Panel
 
-var _rich_label : RichTextLabel
+var _rich_label       : RichTextLabel
+var _clipboard_button : Button
 
 func _ready() -> void:
     _init_variables()
@@ -19,12 +27,17 @@ func _ready() -> void:
 
 func append_text(p_text : String) -> void:
     _rich_label.add_text(p_text + "\n")
-
+    _clipboard_button.visible = true
 
 func clear() -> void:
     _rich_label.clear()
+    _clipboard_button.visible = false
 
 
 func _init_variables() -> void:
-    _rich_label = find_node("RichLabel")
+    _rich_label       = find_node("RichLabel")
+    _clipboard_button = find_node("ClipboardButton")
 
+func _on_ClipboardButton_pressed() -> void:
+    var Os   := OS
+    Os.set_clipboard(_rich_label.text)
